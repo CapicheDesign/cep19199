8c67bf12af1f5898c7c95dd0c12fbdf5171ccc7a
Simon <simon@Simon-iMac.local>
2019-08-16 10:46:07 +1200

    updates to hover states om homepage

--- includes/homeBusiness.php
+++ includes/homeBusiness.php
@@ -1,24 +1,24 @@
 
 
 <section data-index=3 id="business">
-    <div id="business-panel-1" class="panel__home panel__half">
+    <a href="#" id="business-panel-1" class="panel__home panel__half">
         <div class="panel--content">
             <span class="mid-size-bold">Inland Hubs</span>
         </div>
-        <a class="arrow-yellow-fwd" href="#"></a>
-    </div>
-    <div id="business-panel-2" class="panel__home panel__quarter">
+        <span class="arrow-yellow-fwd" href="#"></span>
+    </a>
+    <a href="#" id="business-panel-2" class="panel__home panel__quarter">
         <div class="panel--content">
             <span class="mid-size-bold">Chair&#39;s Report</span>
         </div>
-        <a class="arrow-white-fwd" href="#"></a>
-    </div>
-    <div id="business-panel-3" class="panel__home panel__quarter">
+        <span class="arrow-white-fwd" href="#"></span>
+    </a>
+    <a href="#" id="business-panel-3" class="panel__home panel__quarter">
         <div class="panel--content">
             <span class="mid-size-bold">CEO Report</span>
         </div>
-        <a class="arrow-white-fwd" href="#"></a>
-    </div>
+        <span class="arrow-white-fwd" href="#"></span>
+    </a>
     <div id="business-panel-4" class="panel__home panel__half panel__half-height black-bg">
         <div class="panel--content">
             <div role="presentation" class="panel--icon__container"></div>
@@ -35,7 +35,7 @@
                     </ul>
                 </nav>
             </div>
-            <a class="arrow-yellow-fwd" id="bau-link" href="#"></a>
+            <span class="arrow-yellow-fwd" id="bau-link" href="#"></span>
         </div>
     </div>
 </section>
--- includes/homeHighlights.php
+++ includes/homeHighlights.php
@@ -1,18 +1,18 @@
 <section data-index=2 id="highlights">
-    <div id="highlights-panel-1" class="panel__home panel__third light-blue-bg">
+    <a href="#" id="highlights-panel-1" class="panel__home panel__third light-blue-bg">
         <div class="panel--content">
             <span class="large-text">$67.7m</span>
             <span class="mid-size-text">Revenue up 14% Increased volumes</span>
         </div>
-        <a class="arrow-yellow-fwd" href="#"></a>
-    </div>
-    <div id="highlights-panel-2" class="panel__home panel__third">
+        <span class="arrow-yellow-fwd"></span>
+    </a>
+    <a href="#" id="highlights-panel-2" class="panel__home panel__third">
         <div class="panel--content">
             <span class="mid-size-text">We're serious about safety</span>
         </div>
-        <a class="arrow-yellow-fwd" href="#"></a>
-    </div>
-    <div id="highlights-panel-3" class="panel__home panel__third yellow-bg" >
+        <span class="arrow-yellow-fwd"></span>
+    </a>
+    <a href="#" id="highlights-panel-3" class="panel__home panel__third yellow-bg" >
         <div class="panel--content">
             <span class="mid-size-text">Strong trades</span>
             <div class="panel--content--inner">
@@ -30,26 +30,26 @@
                 </div>
             </div>
         </div>
-        <a class="arrow-white-fwd" href="#"></a>
-    </div>
-    <div id="highlights-panel-4" class="panel__home panel__third">
+        <span class="arrow-white-fwd" href="#"></span>
+    </a>
+    <a href="#" id="highlights-panel-4" class="panel__home panel__third">
         <div class="panel--content">
             <span class="mid-size-text">Our People</span>
         </div>
-        <a class="arrow-yellow-fwd" href="#"></a>
-    </div>
-    <div id="highlights-panel-5" class="panel__home panel__third white-bg">
+        <span class="arrow-yellow-fwd" href="#"></span>
+    </a>
+    <a href="#" id="highlights-panel-5" class="panel__home panel__third white-bg">
         <div class="panel--content">
             <div role="presentation" class="panel--icon__worker"></div>
             <span class="mid-size-text">Regeneration strategy – medium-term plan in action</span>
         </div>
-        <a class="arrow-yellow-fwd" href="#"></a>
-    </div>
-    <div id="highlights-panel-6" class="panel__home panel__third black-bg">
+        <span class="arrow-yellow-fwd" href="#"></span>
+    </a>
+    <a href="#" id="highlights-panel-6" class="panel__home panel__third black-bg">
         <div class="panel--content">
             <div role="presentation" class="panel--icon__leaf"></div>
             <span class="mid-size-text">Commitment to sustainability and our environment</span>
         </div>
-        <a class="arrow-yellow-fwd" href="#"></a>
-    </div>
+        <span class="arrow-yellow-fwd" href="#"></span>
+    </a>
 </section>
